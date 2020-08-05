8d33b56a917e9d932647db1d0823d06e14d92a2f
Chris Moos <chris@chrismoos.com>
2020-08-04 22:55:04 -0700

    Add a ReadWriteInterface for displays.

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,8 @@ pub enum DisplayError {
     InvalidFormatError,
     /// Unable to write to bus
     BusWriteError,
+    // Unable to read from bus
+    BusReadError,
     /// Unable to assert or de-assert data/command switching signal
     DCError,
     /// Unable to assert chip select signal
@@ -56,4 +58,75 @@ pub trait WriteOnlyDataCommand {
 
     /// Send pixel data to display
     fn send_data(&mut self, buf: DataFormat<'_>) -> Result<(), DisplayError>;
+}
+
+#[derive(Clone, Debug)]
+pub enum WriteMode {
+    Data,
+    Command,
+}
+
+pub trait ReadWriteInterface<DataFormat> {
+    fn write(&mut self, mode: WriteMode, buf: &[DataFormat]) -> Result<(), DisplayError> {
+        self.write_iter(mode, &mut buf.into_iter())
+    }
+
+    fn read(&mut self, buf: &mut [DataFormat]) -> Result<(), DisplayError> {
+        let mut n = 0;
+        self.read_stream(&mut |b| {
+            if n == buf.len() {
+                return false;
+            }
+            buf[n] = b;
+            n += 1;
+            true
+        })
+    }
+
+    fn read_stream(&mut self, f: &mut dyn FnMut(DataFormat) -> bool) -> Result<(), DisplayError>;
+
+    fn write_iter(
+        &mut self,
+        mode: WriteMode,
+        iter: &mut dyn Iterator<Item = &DataFormat>,
+    ) -> Result<(), DisplayError>;
+}
+
+pub struct ReadIterator<'a, DataFormat> {
+    rw: &'a mut dyn ReadWriteInterface<DataFormat>,
+}
+
+impl<'a, DataFormat> ReadIterator<'a, DataFormat> {
+    fn new(rw: &'a mut dyn ReadWriteInterface<DataFormat>) -> ReadIterator<'a, DataFormat> {
+        ReadIterator { rw: rw }
+    }
+}
+
+impl<'a, DataFormat> Iterator for ReadIterator<'a, DataFormat>
+where
+    DataFormat: Default,
+{
+    type Item = Result<DataFormat, DisplayError>;
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut next: DataFormat = Default::default();
+        match self.rw.read_stream(&mut |b| {
+            next = b;
+            false
+        }) {
+            Ok(_) => Some(Ok(next)),
+            Err(e) => Some(Err(e)),
+        }
+    }
+}
+
+impl<'a, DataFormat> IntoIterator for &'a mut dyn ReadWriteInterface<DataFormat>
+where
+    DataFormat: Default,
+{
+    type Item = Result<DataFormat, DisplayError>;
+    type IntoIter = ReadIterator<'a, DataFormat>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        ReadIterator::new(self)
+    }
 }
