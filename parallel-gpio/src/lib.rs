a35e1b7bc53821766b2c1c89ff76074aa659857d
Grant Miller <GrantM11235@gmail.com>
2021-05-09 15:31:18 -0500

    parallel-gpio: Add Generic16BitBus

--- parallel-gpio/src/lib.rs
+++ parallel-gpio/src/lib.rs
@@ -108,6 +108,30 @@ generic_bus! {
     }
 }
 
+generic_bus! {
+    Generic16BitBus {
+        type Word = u16;
+        Pins {
+            P0 => 0,
+            P1 => 1,
+            P2 => 2,
+            P3 => 3,
+            P4 => 4,
+            P5 => 5,
+            P6 => 6,
+            P7 => 7,
+            P8 => 8,
+            P9 => 9,
+            P10 => 10,
+            P11 => 11,
+            P12 => 12,
+            P13 => 13,
+            P14 => 14,
+            P15 => 15,
+        }
+    }
+}
+
 /// Parallel communication interface
 ///
 /// This interface implements an "8080" style write-only display interface using any
